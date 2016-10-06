1397a434118bdaa08038ccd2307f313dfc10570e
Mike Bronner <mike@genealabs.com>
2016-10-06 14:08:05 -0700

    Add radio condition

--- resources/views/components/bootstrap3/control.blade.php
+++ resources/views/components/bootstrap3/control.blade.php
@@ -1,6 +1,6 @@
 @include('genealabs-laravel-casts::components.bootstrap3.form-group-open')
 
-@if($type !== 'checkbox' && $type !== 'submit')
+@if($type !== 'checkbox' && $type !== 'radio' && $type !== 'submit')
     @include('genealabs-laravel-casts::components.bootstrap3.label')
 @endif
 
