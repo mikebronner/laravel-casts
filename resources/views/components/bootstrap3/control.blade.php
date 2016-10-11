0ef96ef8fe1069f68ec3435f0679ae579f36c5fa
Mike Bronner <mike@genealabs.com>
2016-10-11 14:51:12 -0700

    Exclude signature from having a label

--- resources/views/components/bootstrap3/control.blade.php
+++ resources/views/components/bootstrap3/control.blade.php
@@ -1,6 +1,6 @@
 @include('genealabs-laravel-casts::components.bootstrap3.form-group-open')
 
-@if($type !== 'checkbox' && $type !== 'radio' && $type !== 'submit')
+@if($type !== 'checkbox' && $type !== 'radio' && $type !== 'submit' && $type !== 'signature')
     @include('genealabs-laravel-casts::components.bootstrap3.label')
 @endif
 
