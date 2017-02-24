910a6b1463e0b9fa4113ae25921fc37ecef05332
Mike Bronner <mike@genealabs.com>
2017-02-24 06:59:03 -0800

    Update CurrentOrLtsLaravelVersion.php

--- src/Traits/CurrentOrLtsLaravelVersion.php
+++ src/Traits/CurrentOrLtsLaravelVersion.php
@@ -11,7 +11,7 @@ trait CurrentOrLtsLaravelVersion
         }
 
         $laravelVersion = app()->version();
-        $currentOrLts = starts_with($laravelVersion, '5.3.')
+        $currentOrLts = starts_with($laravelVersion, '5.3.') || starts_with($laravelVersion, '5.4.')
             ? 'ForLaravelCurrent'
             : (starts_with($laravelVersion, '5.1.') ? 'ForLaravelLts' : '');
 
