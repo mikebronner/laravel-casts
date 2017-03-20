df615c98ded36344f6813e21dec0af77eb65220c
Mike Bronner <mike@genealabs.com>
2017-03-19 18:27:23 -0700

    Fixed file upload control sizing

--- resources/views/components/bootstrap4/file.blade.php
+++ resources/views/components/bootstrap4/file.blade.php
@@ -1,13 +1,9 @@
 @if($isHorizontal)
-<div class="col-sm-{{ $fieldWidth }}">
+<div class="col-sm-{{ $fieldWidth }} custom-file">
 @endif
 
-    <div>
-        <label class="custom-file">
-            {!! $controlHtml !!}
-            <span class="custom-file-control"></span>
-        </label>
-    </div>
+    {!! $controlHtml !!}
+    <span class="custom-file-control" style="right: 15px; left: 15px;"></span>
 
     @if(! $errors->isEmpty() && $errors->has($name))
         <small class="form-text text-danger">{{ implode(' ', $errors->get($name)) }}</small>
