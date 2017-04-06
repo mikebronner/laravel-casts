e4dfdba3c2b263966b25b161714482d7bd9c6b02
Mike Bronner <mike@genealabs.com>
2017-04-05 19:50:11 -0700

    Add combobox to bootstrap4

---
+++ resources/views/components/bootstrap4/combobox.blade.php
@@ -0,0 +1,33 @@
+@if($isHorizontal)
+    <div class="col-sm-{{ $fieldWidth }}">
+@endif
+
+    {!! $controlHtml !!}
+
+    @if(! $errors->isEmpty() && ! $errors->has($name))
+        <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true" style="margin-right: 35px;"></span>
+        <span id="inputSuccess2Status" class="sr-only">(success)</span>
+    @endif
+
+    @if(! $errors->isEmpty() && $errors->has($name))
+        <span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true" style="margin-right: 35px;"></span>
+        <span id="inputError2Status" class="sr-only">(error)</span>
+        <span class="help-block">{{ implode(' ', $errors->get($name)) }}</span>
+    @endif
+    <script>
+        window['genealabsLaravelCasts'] = window.genealabsLaravelCasts || {};
+        window.genealabsLaravelCasts['comboboxLoaders'] = window.genealabsLaravelCasts.comboboxLoaders || [];
+        window.genealabsLaravelCasts.comboboxLoaders.push(function () {
+            $('[name="{{ $name }}"]').selectize({
+                options: {!! $options['list'] !!},
+                list: {!! $options['selected'] !!},
+                labelField: 'text',
+                valueField: 'value',
+                create: {{ $options['createFunction'] }}
+            });
+        });
+    </script>
+
+@if($isHorizontal)
+    </div>
+@endif
