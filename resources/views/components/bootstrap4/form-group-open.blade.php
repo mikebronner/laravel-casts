16883d88eb70f43606eed88437369f80d3d5ede0
Mike Bronner <mike@genealabs.com>
2017-04-12 17:27:28 -0700

    WIP

--- resources/views/components/bootstrap4/form-group-open.blade.php
+++ resources/views/components/bootstrap4/form-group-open.blade.php
@@ -1,1 +1,1 @@
-<div class="form-group {{ $isHorizontal ? 'row' : '' }} {{ ! $errors->isEmpty() ? ($errors->has($name) ? 'has-danger' : 'has-success' ) : '' }} {$addditionalClasses}">
+<div class="form-group {{ $isHorizontal ? 'row' : '' }} {{ ! $errors->isEmpty() ? ($errors->has($name) ? 'has-danger' : 'has-success' ) : '' }} {{ $addditionalClasses }}">
