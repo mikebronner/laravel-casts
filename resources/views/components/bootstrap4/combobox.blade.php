cbba6373aca5490853ff00d2f44048c6f3e3d112
Mike Bronner <mike@genealabs.com>
2017-08-29 14:37:22 -0700

    remove glyphicons from bootstrap 4 in combobox

--- resources/views/components/bootstrap4/combobox.blade.php
+++ resources/views/components/bootstrap4/combobox.blade.php
@@ -5,14 +5,12 @@
     {!! $controlHtml !!}
 
     @if(! $errors->isEmpty() && ! $errors->has($name))
-        <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true" style="margin-right: 35px;"></span>
         <span id="inputSuccess2Status" class="sr-only">(success)</span>
     @endif
 
     @if(! $errors->isEmpty() && $errors->has($name))
-        <span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true" style="margin-right: 35px;"></span>
         <span id="inputError2Status" class="sr-only">(error)</span>
-        <span class="help-block">{{ implode(' ', $errors->get($name)) }}</span>
+        <span class="invalid-feedback">{{ implode(' ', $errors->get($name)) }}</span>
     @endif
     <script>
         window['genealabsLaravelCasts'] = window.genealabsLaravelCasts || {};
