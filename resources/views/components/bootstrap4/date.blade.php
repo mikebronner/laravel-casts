6b2ba876cc6eea5a13af9eb6ecb4337dceb97c59
Mike Bronner <mike@genealabs.com>
2017-08-30 14:44:06 -0700

    Update date.blade.php

--- resources/views/components/bootstrap4/date.blade.php
+++ resources/views/components/bootstrap4/date.blade.php
@@ -1,37 +1,39 @@
 @if($isHorizontal)
-<div class="col-sm-{{ $fieldWidth }}">
+    <div class="col-sm-{{ $fieldWidth }}">
 @endif
 
-    <div class="input-group date" id="datetimepicker-{{ $name }}" data-target-input="nearest">
-        {!! $controlHtml !!}
-        <span class="input-group-addon" data-target="#datetimepicker-{{ $name }}" data-toggle="datetimepicker">
-            <i class="fa fa-btn fa-calendar"></i>
-        </span>
-    </div>
+<div class="input-group date" id="datetimepicker-{{ $name }}" data-target-input="nearest">
+    {!! $controlHtml !!}
+    <span class="input-group-addon" data-target="#datetimepicker-{{ $name }}" data-toggle="datetimepicker">
+        <i class="fa fa-btn fa-calendar"></i>
+    </span>
+</div>
 
-    <script>
-        window['genealabsLaravelCasts'] = window.genealabsLaravelCasts || {};
-        window.genealabsLaravelCasts['dateTimeLoaders'] = window.genealabsLaravelCasts.dateTimeLoaders || [];
-        window.genealabsLaravelCasts.dateTimeLoaders.push(function () {
-            $("#datetimepicker-{{ $name }}").datetimepicker({
-                format: 'll',
-                date: '{{ $options['value'] }}',
-                icons: {
-                    time: "fa fa-btn fa-clock-o",
-                    date: "fa fa-btn fa-calendar",
-                    up: "fa fa-btn fa-arrow-up",
-                    down: "fa fa-btn fa-arrow-down"
-                },
-                locale: "{{ config('app.locale') }}",
-                widgetPositioning: {horizontal: 'left'}
-            });
+<script>
+    window['genealabsLaravelCasts'] = window.genealabsLaravelCasts || {};
+    window.genealabsLaravelCasts['framework'] = 'bootstrap4';
+    window.genealabsLaravelCasts['dateTimeLoaders'] = window.genealabsLaravelCasts.dateTimeLoaders || [];
+    window.genealabsLaravelCasts.dateTimeLoaders.push(function () {
+        $("#datetimepicker-{{ $name }}").datetimepicker({
+            format: 'll',
+            date: '{{ $options['value'] }}',
+            sideBySide: true,
+            icons: {
+                time: "fa fa-btn fa-clock-o",
+                date: "fa fa-btn fa-calendar",
+                up: "fa fa-btn fa-arrow-up",
+                down: "fa fa-btn fa-arrow-down"
+            },
+            locale: "{{ config('app.locale') }}",
+            widgetPositioning: {horizontal: 'left'}
         });
-    </script>
+    });
+</script>
 
-    @if(! $errors->isEmpty() && $errors->has($name))
-        <div class="invalid-feedback">{{ implode(' ', $errors->get($name)) }}</div>
-    @endif
+@if(! $errors->isEmpty() && $errors->has($name))
+    <div class="invalid-feedback">{{ implode(' ', $errors->get($name)) }}</div>
+@endif
 
 @if($isHorizontal)
-</div>
+    </div>
 @endif
