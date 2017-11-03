29cf7ba8d21700bffeebab7079b9c01af023e61f
Mike Bronner <mike@genealabs.com>
2017-11-03 15:30:34 -0700

    WIP - vue components

---
+++ resources/assets/js/components/bootstrap4/cast-hidden.vue
@@ -0,0 +1,22 @@
+<script>
+    module.exports = {
+        name: "cast-hidden",
+
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+
+            value: {
+                type: String,
+                required: false,
+                default: '',
+            },
+        },
+    };
+</script>
+
+<template>
+    <input type="hidden" :name="name" :value="value">
+</template>
---
+++ resources/assets/js/components/casts.js
@@ -0,0 +1,3 @@
+Vue.component('cast-form', require('./bootstrap4/cast-form.vue'));
+Vue.component('cast-hidden', require('./bootstrap4/cast-hidden.vue'));
+Vue.component('cast-checkbox', require('./bootstrap4/cast-checkbox.vue'));
