297d113bd72d6ca934b2f6901d2a0a2b59e8b190
Mike Bronner <mike@genealabs.com>
2017-11-04 15:05:18 -0700

    Remove vue components

--- resources/assets/js/components/casts.js
+++
@@ -1,3 +0,0 @@
-Vue.component('cast-form', require('./bootstrap4/cast-form.vue'));
-Vue.component('cast-hidden', require('./bootstrap4/cast-hidden.vue'));
-Vue.component('cast-checkbox', require('./bootstrap4/cast-checkbox.vue'));
