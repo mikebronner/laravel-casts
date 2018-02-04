7833f09d88b7c826458d234c0ee6aad67dea8118
Mike Bronner <mike@genealabs.com>
2018-02-03 19:35:35 -0800

    Switch from glyphicons to fontawesome

--- resources/views/components/bootstrap3/input.blade.php
+++ resources/views/components/bootstrap3/input.blade.php
@@ -5,12 +5,12 @@
 {!! $controlHtml !!}
 
 @if(! $errors->isEmpty() && ! $errors->has($name))
-    <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+    <span class="fa fa-check form-control-feedback" aria-hidden="true"></span>
     <span id="inputSuccess2Status" class="sr-only">(success)</span>
 @endif
 
 @if(! $errors->isEmpty() && $errors->has($name))
-    <span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>
+    <span class="fa fa-times form-control-feedback" aria-hidden="true"></span>
     <span id="inputError2Status" class="sr-only">(error)</span>
     <span class="help-block">{{ implode(' ', $errors->get($name)) }}</span>
 @endif
