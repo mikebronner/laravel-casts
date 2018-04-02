755d8533057e333e4f88d41cc7a1286b2680ac3d
Mike Bronner <mike@genealabs.com>
2018-04-02 10:54:57 -0700

    Update File.php

--- src/File.php
+++ src/File.php
@@ -4,6 +4,12 @@ class File extends ValuelessInput
 {
     public function __construct(string $name, $value = null, array $options = [])
     {
+        if (! array_key_exists("label", $options)
+            || trim($options["label"]) === ""
+        ) {
+            $options["label"] = "Choose File";
+        }
+
         parent::__construct($name, $value, $options);
 
         if ($this->framework === 'bootstrap3') {
