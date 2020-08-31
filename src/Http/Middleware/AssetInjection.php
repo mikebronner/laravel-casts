a885ad21d99e9e58960e56502286b0143fd2f5be
Mike Bronner <mike@genealabs.com>
2020-08-31 09:56:19 -0700

    Update AssetInjection.php

--- src/Http/Middleware/AssetInjection.php
+++ src/Http/Middleware/AssetInjection.php
@@ -21,8 +21,6 @@ class AssetInjection
         }
 
         $castsScripts = '<script src="' . asset('genealabs-laravel-casts/app.js') . '"></script></body>';
-        $livewireScripts = (new LivewireManager)->scripts();
-        $livewireStyles = (new LivewireManager)->styles();
 
         $html = (new HtmlPageCrawler)->create($content);
 
@@ -31,8 +29,6 @@ class AssetInjection
         }
 
         if ($html->filter("html > head")->count()) {
-            $html->filter("html > head")->append($livewireStyles);
-            $html->filter("html > body")->append($livewireScripts);
             $html->filter("html > body")->append($castsScripts);
             $content = $html->saveHTML();
         }
