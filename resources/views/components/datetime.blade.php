a6157976bcf926c16b0c71f8cb3829882d4cd59e
Mike Bronner <mike.bronner@totalitylms.com>
2021-03-17 13:55:28 +0000

    WIP

--- resources/views/components/datetime.blade.php
+++ resources/views/components/datetime.blade.php
@@ -11,8 +11,8 @@
     @endif
 
     <input
-        {{ $attributes->merge(["class" => "form-input"]) }}
-        type="datetime-local"
+        {{ $attributes->merge(["class" => "z-10 focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md pl-4 sm:text-sm border-gray-300"]) }}
+        type="datetime"
         id="{{ $name }}"
         name="{{ $name }}"
         value="{{ $value }}"
