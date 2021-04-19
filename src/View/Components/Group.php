01985d1d31939607398cb9875922588106543614
Mike Bronner <mike@genealabs.com>
2021-04-19 12:17:45 -0700

    WIP

--- src/View/Components/Group.php
+++ src/View/Components/Group.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace GeneaLabs\LaravelCasts\View\Components;
 
 use Illuminate\View\Component;
@@ -9,13 +11,16 @@ class Group extends Component
 {
     public array $errorData;
     public string $helpText;
+    public string $name;
 
     public function __construct(
+        string $name,
         array $errorData = [],
         string $helpText = "",
     ) {
         $this->errorData = $errorData;
         $this->helpText = $helpText;
+        $this->name = $name;
     }
 
     public function render(): View
