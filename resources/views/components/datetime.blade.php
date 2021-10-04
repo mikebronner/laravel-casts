d79dfefe86f54e129c944bd9e7a3cd3b15e80fd6
Mike Bronner <mike@genealabs.com>
2021-10-04 22:15:00 +0000

    Fixed datetime field.

--- resources/views/components/datetime.blade.php
+++ resources/views/components/datetime.blade.php
@@ -11,15 +11,15 @@
     @endif
 
     <input
-        {{ $attributes->merge(["class" => "z-10 focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md pl-4 sm:text-sm border-gray-300"])->except(['x-show', 'x-if']) }}
-        type="datetime"
+        {{ $attributes->merge(["class" => "form-input"])->except(['x-show', 'x-if']) }}
+        type="datetime-local"
         id="{{ $name }}"
         name="{{ $name }}"
         value="{{ $value }}"
     >
 
     @error($nameInDotNotation)
-        <p class="mt-1 text-red-600 text-sm">
+        <p class="mt-1 text-sm text-red-600">
             {{ str_replace($nameInDotNotation, "'{$label}'", $message) }}
         </p>
     @enderror
