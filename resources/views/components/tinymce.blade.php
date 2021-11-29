99e9aedf86a207816e3a014d843b644fc122898e
Mike Bronner <mike@genealabs.com>
2021-11-29 19:34:47 +0000

    Updated tinymce editor configuration.

--- resources/views/components/tinymce.blade.php
+++ resources/views/components/tinymce.blade.php
@@ -36,9 +36,6 @@
     >
         {{ $helpText }}
     </span>
-</x-form-group>
-
-@push ("js")
     {{-- Todo: add npm package instead --}}
     <script
         src="https://cdn.tiny.cloud/1/{{ config("services.tinymce.api-key", "please-add-your-api-key") }}/tinymce/5/tinymce.min.js"
@@ -58,21 +55,48 @@
 
                     tinymce.init({
                         selector: '#{{ $name }}',
-                        {{-- menubar: 'edit view insert format tools table', --}}
                         forced_root_block : false,
                         menubar: '',
-                        plugins: 'advcode casechange export fullscreen hr linkchecker autolink lists image advlist pagebreak powerpaste searchreplace table advtable tinymcespellchecker wordcount',
-                        toolbar: 'undo redo | fontselect fontsizeselect | bold italic underline strikethrough superscript subscript removeformat pagebreak hr wordcount | align | outdent indent | bullist numlist table | casechange searchreplace | spellchecker spellcheckdialog | code export fullscreen',
+                        plugins: 'advcode casechange export fullscreen hr autolink lists image advlist pagebreak powerpaste searchreplace table advtable wordcount',
+                        toolbar: 'undo redo | styleselect | bold italic underline strikethrough superscript subscript removeformat pagebreak hr wordcount | align | outdent indent | bullist numlist table image | casechange searchreplace | spellchecker spellcheckdialog | code export fullscreen',
                         toolbar_mode: 'sliding',
                         init_instance_callback: function (editor) {
-                            editor.on('Change Undo Redo Paste', function (e) {
+                            editor.on('KeyUp Change Undo Redo Paste', function (e) {
                                 self.changed(tinymce.activeEditor.getContent());
                             });
+
+                            editor.on("drop", function(e) {
+                                if (e.dataTransfer.items) {
+                                    for (let i = 0; i < e.dataTransfer.items.length; i++) {
+                                        if (e.dataTransfer.items[i].kind === 'file') {
+                                            e.preventDefault();
+
+                                            let file = e.dataTransfer.items[i].getAsFile();
+
+                                            self.dropHandler(editor, file);
+                                        }
+                                    }
+                                } else {
+                                    for (let i = 0; i < e.dataTransfer.files.length; i++) {
+                                        e.preventDefault();
+
+                                        let file = e.dataTransfer.items[i].getAsFile();
+
+                                        self.dropHandler(editor, file);
+                                    }
+                                }
+                            });
                         },
+                        relative_urls : false,
+                        document_base_url : "{ url('/'); }",
+                        images_upload_handler: this.imageUploadHandler,
                         max_height: 600,
                         branding: false,
-                        paste_data_images: true,
-                        pagebreak_separator: '<div style="page-break-before: always;"></div>'
+                        paste_data_images: false,
+                        pagebreak_separator: '<div style="page-break-before: always;"></div>',
+                        images_dataimg_filter: function (img) {
+                            return !img.hasAttribute('internal-blob');
+                        }
                     });
 
                     this.dispatch("editor", this.editor);
@@ -81,7 +105,62 @@
                 changed: function (data) {
                     this.dispatch("input", data);
                 },
+
+                dropHandler: function (editor, file) {
+                    const reader = new FileReader();
+
+                    reader.onloadend = function () {
+                        editor.execCommand('mceInsertContent', false, `<img src="${reader.result}"></img>`);
+                    };
+
+                    reader.readAsDataURL(file);
+                },
+
+                imageUploadHandler: function (blobInfo, success, failure, progress) {
+                    var xhr, formData;
+
+                    xhr = new XMLHttpRequest();
+                    xhr.withCredentials = false;
+                    xhr.open('POST', '{{ $uploadPath }}');
+                    xhr.setRequestHeader("X-CSRF-TOKEN", "{{ csrf_token() }}")
+
+                    xhr.upload.onprogress = function (e) {
+                        progress(e.loaded / e.total * 100);
+                    };
+
+                    xhr.onload = function() {
+                        var json;
+
+                        if (xhr.status === 403) {
+                            failure('HTTP Error: ' + xhr.status, { remove: true });
+                            return;
+                        }
+
+                        if (xhr.status < 200 || xhr.status >= 300) {
+                            failure('HTTP Error: ' + xhr.status);
+                            return;
+                        }
+
+                        json = JSON.parse(xhr.responseText);
+
+                        if (!json || typeof json.location != 'string') {
+                            failure('Invalid JSON: ' + xhr.responseText);
+                            return;
+                        }
+
+                        success(json.location);
+                    };
+
+                    xhr.onerror = function () {
+                        failure('Image upload failed due to a XHR Transport error. Code: ' + xhr.status);
+                    };
+
+                    formData = new FormData();
+                    formData.append('file', blobInfo.blob(), blobInfo.filename());
+
+                    xhr.send(formData);
+                },
             };
         }
     </script>
-@endpush
+</x-form-group>
--- src/View/Components/TinyMce.php
+++ src/View/Components/TinyMce.php
@@ -1,8 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
 namespace GeneaLabs\LaravelCasts\View\Components;
 
 class TinyMce extends Textarea
 {
-    //
+    public $uploadPath;
+
+    public function __construct(
+        string $name,
+        ?string $value = null,
+        ?string $labelClasses = "",
+        ?string $groupClasses = "",
+        ?string $errorClasses = "",
+        ?string $label = null,
+        ?string $helpClasses = "",
+        ?string $helpText = "",
+        ?string $uploadPath = "",
+    ) {
+        $this->uploadPath = $uploadPath;
+
+        parent::__construct($name, $value, $label, $labelClasses, $groupClasses, $errorClasses, $helpClasses, $helpText);
+    }
 }
