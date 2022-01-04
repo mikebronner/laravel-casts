da0dd1d877479343c1a1bdfbacf7c5a8ebb7af73
Mike Bronner <mike@genealabs.com>
2022-01-04 14:39:55 +0000

    Fixed css syntax.

--- resources/views/components/group.blade.php
+++ resources/views/components/group.blade.php
@@ -1,8 +1,9 @@
 @push ("css")
     <style>
+        .form-checkbox[disabled="disabled"] + p.text-red-600,
         .form-input[disabled="disabled"] + p.text-red-600,
         .form-select[disabled="disabled"] + p.text-red-600,
-        .form-textarea[disabled="disabled"] + p.text-red-600,
+        .form-textarea[disabled="disabled"] + p.text-red-600
         {
             display: none;
         }
