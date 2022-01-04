54bfcd8e0fa30d16bfe86b1c2783e32f3b1816e0
Mike Bronner <mike@genealabs.com>
2022-01-04 15:50:23 +0000

    WIP

--- src/View/Components/BaseComponent.php
+++ src/View/Components/BaseComponent.php
@@ -33,7 +33,7 @@ abstract class BaseComponent extends Component
         $this->uniqueId = Str::random(16);
         $this->name = $name;
         $this->nameInDotNotation = trim(str_replace("[", ".", str_replace("]", "", $this->name)), ".");
-        $this->value = isset($value)
+        $this->value = $value !== null
             ?: old($this->nameInDotNotation)
             ?: data_get(session("form-model"), $this->nameInDotNotation)
             ?: "";
