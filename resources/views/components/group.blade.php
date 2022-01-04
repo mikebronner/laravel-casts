59546028a2e5242c295c158b7fd9287a03ea2456
Mike Bronner <mike@genealabs.com>
2022-01-04 14:42:15 +0000

    WIP

--- resources/views/components/group.blade.php
+++ resources/views/components/group.blade.php
@@ -1,4 +1,5 @@
 @push ("css")
+    {{-- TODO: figure out a way to push only once --}}
     <style>
         .form-checkbox[disabled="disabled"] + p.text-red-600,
         .form-input[disabled="disabled"] + p.text-red-600,
