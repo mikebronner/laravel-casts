04ea285df9d1d1de3ea9fb10d6f81d29fc387030
Mike Bronner <mike@genealabs.com>
2022-01-04 14:37:01 +0000

    Hide validation errors for disabled fields.

--- resources/views/components/group.blade.php
+++ resources/views/components/group.blade.php
@@ -1,3 +1,14 @@
+@push ("css")
+    <style>
+        .form-input[disabled="disabled"] + p.text-red-600,
+        .form-select[disabled="disabled"] + p.text-red-600,
+        .form-textarea[disabled="disabled"] + p.text-red-600,
+        {
+            display: none;
+        }
+    </style>
+@endpush
+
 <div
     {{ $attributes }}
 >
