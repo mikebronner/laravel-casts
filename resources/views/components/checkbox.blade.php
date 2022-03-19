2868cb6ea1c1c8764dc6b2bdcea980b0a669cb48
Mike Bronner <mike@genealabs.com>
2022-03-19 21:11:29 +0000

    Fixed checkbox.

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -6,15 +6,13 @@
 >
     <input
         {{ $attributes->merge(['class' => 'form-checkbox'])->whereDoesntStartWith(['x-']) }}
-        x-data
-        x-ref="input"
-        x-on:change="$dispatch('input', this.value)"
-        id="{{ trim($name, "[]") }}[{{ $value }}]"
         type="checkbox"
+        @if (! $attributes->wire('model'))
+        id="{{ trim($name, "[]") }}[{{ $value }}]"
         name="{{ $name }}"
         value="{{ $value }}"
         {{ $checked }}
-        wire:ignore
+        @endif
     >
 
     @error($nameInDotNotation)
