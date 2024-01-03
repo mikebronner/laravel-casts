65b4bd0e52591d24d352174ae51a2fb90eafb22e
Mike Bronner <mike@genealabs.com>
2024-01-03 18:35:49 +0000

    WIP

--- src/Tel.php
+++ src/Tel.php
@@ -1,6 +1,27 @@
 <?php namespace GeneaLabs\LaravelCasts;
 
+use Brick\PhoneNumber\PhoneNumber;
+use Illuminate\Support\Str;
+
 class Tel extends Input
 {
+    public string $countryCode;
+    public string $format;
 
+    public function __construct(
+        public string $country,
+        string $name,
+        string $value = null,
+    ) {
+        parent::__construct($name, $value, []);
+        $examplePhoneNumber = PhoneNumber::getExampleNumber($country);
+        $this->countryCode = $examplePhoneNumber->getCountryCode();
+        $this->format = $examplePhoneNumber->formatForCallingFrom($country);
+        $this->format = trim(Str::replace($this->countryCode, "", $this->format));
+        $this->format = preg_replace("/\d/", "9", $this->format);
+
+        if ($this->framework === 'tailwind') {
+            $this->classes .= ' form-input mt-1 block w-full';
+        }
+    }
 }
